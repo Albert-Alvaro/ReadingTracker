9531ba8a272bd0338fea8ee3403c0f87ed8460cd
Albert Alvaro <118112483+Albert-Alvaro@users.noreply.github.com>
2023-01-15 12:53:55 +0800

    started creating book class

--- book.py
+++ book.py
@@ -6,4 +6,20 @@
 
 class Book:
     """..."""
-    pass
+    def __init__(self, title="", author="", page_number=0, is_required=""):
+        self.title = title
+        self.author = author
+        self.page_number = page_number
+        self.is_required = is_required
+
+    def mark_required(self):
+        if self.is_required == "r":
+            return True
+
+    def mark_complete(self):
+        if self.is_required == "c":
+            return False
+
+    def is_long(self):
+        if self.page_number >= 500:
+            return "Long"
